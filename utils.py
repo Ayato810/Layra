d181fc119278c5903caeca9641f53968fba66f58
Ayato <manhattancafe0810520@gmail.com>
2023-10-14 13:35:37 -0500

    basic boilerplate code done

--- utils.py
+++ utils.py
@@ -1,15 +1,32 @@
 import re
 
+from NovelAILLMWrapper.GenerationSettings import GenerationSettings
+from novelai_api.Preset import Preset, Model
+from novelai_api.BanList import BanList
+
 
 def get_final_prompt(prompt, user, char):
     replacements = {
         "{{user}}": user,
         "{{char}}": char
     }
-
-    final_prompt = ""
     for placeholder, replacement in replacements.items():
         pattern = re.escape(placeholder)
-        final_prompt = re.sub(pattern, replacement, prompt)
+        prompt = re.sub(pattern, replacement, prompt)
+    return prompt
 
-    return final_prompt
+
+def load_generation_settings(config_data):
+    preset = Preset.from_official(Model.Kayra, "ProWriter")
+    preset.min_length = 1
+    preset.max_length = 150
+    bad_words = BanList()
+    for item in config_data["bad_words"]:
+        bad_words.add(item)
+    user = config_data["user"]
+    char = config_data["char"]
+    stop_sequences = ["\n", f"\n{user}", f"\n{char}"]
+    generation_settings = GenerationSettings(preset=preset, stop_sequences=stop_sequences,
+                                             repetition_penalty_whitelist=config_data["repetition_penalty_whitelist"],
+                                             bad_words=bad_words)
+    return generation_settings
